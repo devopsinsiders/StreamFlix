dfa3bddcb200d0448137a99e8cd6378b6e50ff6d
bot <info@devopsinsiders.com>
2024-05-26 20:28:58 +0530

    added build

--- src/components/layouts/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MainHeader } from "./MainHeader";
-export { default as Footer } from "./Footer";
--- src/store/slices/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-import { API_ENDPOINT_URL } from "src/constant";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-export const tmdbApi = createApi({
-  reducerPath: "tmdbApi",
-  baseQuery: fetchBaseQuery({ baseUrl: API_ENDPOINT_URL }),
-  endpoints: (build) => ({}),
-});
